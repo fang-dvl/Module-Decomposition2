2b6398258ddc814bdbc7fe3bcf03463137811b56
Fanglai <laura@FLMacBook-Air.local>
2026-06-02 23:24:25 +0200

    fixed file name issues

---
+++ backend-with-two-customs-middlewares/backend.js
@@ -0,0 +1,44 @@
+//backend with two custom middlewares
+
+import express from'express';
+import cors from 'cors';
+
+const app=express();
+app.use(cors());
+app.use(express.json());
+
+const PORT=process.env.PORT || 3000;
+
+const messages=[];
+
+const requestLogger = (req, res, next) => {
+    const time = new Date().toISOString();
+    next()
+};
+
+const validateMessage = (req, res, next) => {
+    if (req.method === 'POST') {
+        const { text } = req.body;
+        if (!text || text.trim() === '') {
+            return res.status(400).json({ error: 'Text cannot be empty!' });
+        }
+    }
+    next();
+};
+app.use(requestLogger);
+app.use(validateMessage);
+
+app.get('/messages', (req, res) => {
+    res.json(messages);
+});
+
+app.post('/messages',(req,res) => {
+    const message=req.body;
+    messages.push(message);
+    res.status(201).json(message);
+})
+
+app.listen(PORT,()=> {
+    console.log(`Server is running on port ${PORT}`);
+})
+
